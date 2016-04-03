9c9b3c30b928fad1740a39e64bfa46c7a5cf1bdd
Harendra Singh <66hari@gmail.com>
2016-04-03 16:06:36 +0530

    Updates server to run on port 80

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.get('/*', function (req, res) {
 	res.sendFile( __dirname + '/static/app.html');
 });
 
-app.listen(3000, function () {
-	console.log('Example app listening on port 3000!');
+app.listen(80, function () {
+	console.log('Example app listening on port 80!');
 });
