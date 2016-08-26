1465c6c6c8b05d901d720806b4eb87e97d4b350c
Manisha <manisha@shippable.com>
2016-08-26 13:30:39 -0700

    Update push.sh

--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 if [ "$IS_PULL_REQUEST" != true ]; then
-  sudo docker push $IMAGE_NAME:$BRANCH.1
+  sudo docker push $IMAGE_NAME:$BRANCH.$BUILD_NUMBER
 else
   echo "skipping because it's a PR"
 fi
